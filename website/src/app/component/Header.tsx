573a5f8fcabd6686a584433efcdc82998dcb78ba
AYeshsidd <159470548+AYeshsidd@users.noreply.github.com>
2025-01-28 10:45:42 -0800

    Update Header.tsx

--- website/src/app/component/Header.tsx
+++ website/src/app/component/Header.tsx
@@ -1,4 +1,3 @@
-
 import Image from "next/image";
 import Link from "next/link";
 
@@ -11,44 +10,38 @@ export default function Header(){
 
       <div className="flex flex-col justify-between md:flex-row items-center px-4 md:px-10 py-6">
         <div className="mb-4 md:mb-0 "> 
-            {/* creating space for small only */}
-       
-        <Image src={"/logo.png"} width={185} height={41} alt="LOGO" className=" "/>
+            
+        <Image src={"/logo.png"} width={185} height={41} alt="LOGO"/>
         </div>
 
-        <div className="my-5 w-[430px] h-24 left[505px] mx-6 flex flex-wrap  justify-center space-x-6 md:space-x-10 text-sm md:text-base mb-4 md:mb-0">
-          <Link href="http://localhost:3000">Home</Link>
-          <Link href="./shop">Shop</Link>
-          <Link href="./blog">Blog</Link>
-          <Link href="./contact">Contact</Link> 
+        <div className="my-5 md:w-[430px] md:h-24 left[505px] mx-6 flex flex-wrap justify-center space-x-6 md:space-x-10 text-sm md:text-base mb-4 md:mb-0">
+          <Link href="http://localhost:3000" className=" hover:text-yellow-600">Home</Link>
+          <Link href="./shop" className=" hover:text-yellow-600 ">Shop</Link>
+          <Link href="./blog" className=" hover:text-yellow-600">Blog</Link>
+          <Link href="./contact" className=" hover:text-yellow-600">Contact</Link> 
         </div>
 
-<div className="flex space-x-9 ">  
-   <Link href="./sidebar">
+<div className="flex space-x-9 my-8 ">  
+   
+   <Link href="./sidebar" className="hover:scale-125 hover:-translate-y-3 transition-all">
    <Image src={"/vector.png"} width={23.33} height={18.67} alt="vector man" className=""/>
    </Link>
 
-   <Link href="./comparision"> <Image src={"/search.png"} width={22.17} height={22.17} alt="vector man" className=""/>
+   <Link href="./comparision" className="hover:scale-125 hover:-translate-y-3 transition-all"> <Image src={"/search.png"} width={22.17} height={22.17} alt="vector man" className=""/>
    </Link>
-  
-    <Link href="./singalProduct">
+   <Link href="./singalProduct" className="hover:scale-125 hover:-translate-y-3 transition-all">
     <Image src={"/heart.png"} width={23.33} height={20.81} alt="vector man" className=""/>
     </Link>
  
-  
-  <Link href="./cart">
+  <Link href="./cart" className="hover:scale-125 hover:-translate-y-3 transition-all">
     <Image src={"/cart.png"} width={24.53} height={22.06} alt="vector man" className="" />
   </Link>
 
 </div>
 
-
     </div>
 
-
-
-
-      </>
+   </>
     );
   }
 
